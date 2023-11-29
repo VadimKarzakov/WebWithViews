d2a1590b2bcb9c2f716eb647d1e773bbce6e8cae
Вадим Карзаков <vkarzakov31@gmail.com>
2023-11-29 15:30:23 +0300

    Привет я Дора

---
+++ src/main/java/com/example/springdatabasicdemo/web/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springdatabasicdemo.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String Page() {
+        return "index";
+    }
+
+}
+
+
+
+
